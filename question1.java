77e702683eb37908010e31facb0413e2f52d6878
sumit <kumarsumit43252@gmail.com>
2025-08-27 19:13:28 +0530

    solved

--- question1.java
+++ question1.java
@@ -1,6 +1,98 @@
 public class question1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // question no 1............
+
+        // int a =8,b=4,c=2;
+        // a+=(a+b-c)/2;
+        // System.out.println(a);
+
+        // question no 2............
+        
+        // int n =436;
+        // n/=10;
+        // System.out.println(n);
+
+        // question no 3............
+
+        // int n =36;
+        // n*=2;
+        // n*=2;
+        // System.out.println(n);
+
+        // question no 4............
+
+        // int k =3;
+        // k*=k+=k-1;
+        // System.out.println(k); // output is 15
+
+        // question no 5............
+
+        // int n =14;
+        // n+=n%5*2;
+        // System.out.println(n); // output is 22
+
+        // question no 6............
+
+        // int a,b,c;
+        // a=b=c=5;
+        // a+=b-=c*=2;
+        // System.out.println(a+" "+b+" "+c);  output is 0 -5 10
+
+        // question no 7............
+
+        // int m =27;
+        // m%=5 +4;
+        // System.out.println(m); // output is 0
+
+        // question no 8............
+
+        // int x=5;
+        // x+=x*=2;
+        // System.out.println(x); // output is 15
+
+        // question no 9............
+
+        // int a=2, b=3, c=4;
+        // a+=b +=c*=2;
+        // System.out.println(a+" "+b+" "+c); // output is 13 11 8
+
+        // question no 10............
+
+        // int n =4;
+        // n*=n+=2;
+        // System.out.println(n); // output is 24
+
+        // question no 11............
+
+        // int a,b,c;
+        // a=b=c=5;
+        // a+=b-=c*=2;
+        // System.out.println(a+" "+b+" "+c); // output is 0 -5 10
+
+        // question no 12............
+
+        // int m = 17;
+        // m+=m%3*2;
+        // System.out.println(m); // output is 21
+
+        // question no 13............
+
+        // int a=2,b=3,c=4;
+        // a+=b +=c*=2;
+        // System.out.println(a+" "+b+" "+c); // output is 13 11 8
+
+        // question no 14............
+
+        // int num = 29;
+        // num%=5+3;
+        // System.out.println(num); // output is 5
+
+        // question no 15............
+
+        int a=5, b=3;
+        a+=b*=2;
+        System.out.println(a+" "+b); // output is 11 6
     }
     
 }
